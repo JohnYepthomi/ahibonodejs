a555db91ce56b077e7d46c3806de3cdeea5323cf
JohnYepthomi <johnyepthomi@gmail.com>
2020-11-17 19:21:07 +0000

    Notification input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
     <div className="App">
         <p className="brand animate__animated animate__bounce animate__infinite">AHIBO</p>
         <p className="message animate__animated animate__heartBeat">We're coming soon..</p>
+        <div className="form_container">
+        <form>
+            <p className="notif_msg">Get notified when we're ready</p>
+        <input className="input" type="text" placeholder="Enter your e-mail"/>
+        <button className="notify_btn">Subscribe</button>
+        </form>
+    </div>
     </div>
   );
 }
